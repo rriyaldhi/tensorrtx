87e3f56da8760fc46f3c45b48316c19a9fbdc815
rriyaldhi <rriyaldhi@gmail.com>
2021-06-14 17:24:34 +0200

    test

--- retinaface/retina_r50.cpp
+++ retinaface/retina_r50.cpp
@@ -337,7 +337,7 @@ int main(int argc, char** argv) {
     //for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
     //    data[i] = 1.0;
 
-    cv::Mat img = cv::imread("../images/2.fullhd.jpeg");
+    cv::Mat img = cv::imread("../images/2.hd.jpeg");
     cv::Mat pr_img = preprocess_img(img, INPUT_W, INPUT_H);
     //cv::imwrite("preprocessed.jpg", pr_img);
 
